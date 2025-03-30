29971e1ca8dad702cb1cb59200727b99b16de515
Daniel Ramos <danielramosacosta1@gmail.com>
2025-03-30 13:51:07 +0200

    feat: add ScrollReveal library for enhanced animations; update recipe details and layout styling

--- src/app.js
+++ src/app.js
@@ -1,2 +1,8 @@
-const name = 'world';
-console.log(`Hello ${name}`);
+import ScrollReveal from 'scrollreveal';
+
+ScrollReveal().reveal('.reveal', { 
+    distance: '50px',
+    duration: 1000,
+    origin: 'bottom',
+    interval: 200
+});
